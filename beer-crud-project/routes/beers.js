8d516eb456421a3b9100bfd34caeaf0bdfe91a97
Marten Zand-Vakili <martenzv@hotmail.de>
2022-02-14 14:48:59 +0100

    Set up routes for Beer

--- beer-crud-project/routes/beers.js
+++ beer-crud-project/routes/beers.js
@@ -8,13 +8,73 @@ router.get("/", (req, res, next) => {
     .then(beersFromDB => {
         res.render("beers/beer-collection", {beers: beersFromDB})
     })
-    .catch(err => {console.log("Error getting beers from DB", err)
+    .catch(err => {console.log("Error getting beers from DB.", err)
     })
 });
 
-router.post("/beer-create", (req, res, next) => {
-    const beerDetails
-}
+ router.post("/beer-create", (req, res, next) => {
+     const beerDetails = {
+        title: req.body.title,
+        brewery: req.body.brewery,
+        description: req.body.description,
+        rating: req.body.rating
+     }; 
+     Beer.create(beerDetails)
+    .then(() => {
+      res.redirect("/beer-collection");
+    })
+    .catch( err => {
+      console.log("Error creating new beer.", err);
+    })
+});
 
+router.get("/:beerId", (req, res, next) => {
+    Beer.findById(req.params.beerId)
+    .then( beer => {
+        res.render("beers/beers-details", beer)
+    })
+    .catch( err => {
+        console.log("Error getting beer from DB", err)
+    })
+});
+
+router.get("/:beerId/edit", (req, res, next) =>{
+    Beer.findById(req.params.beerId)
+    .then(beer => {
+        res.render("beers/beer-edit", beer)
+    })
+    .catch( err => {
+        console.log("Error getting beer details from DB", err)
+    })
+});
+
+router.post("/:bookId/edit", (req, res, next) => {
+    const beerId = req.params.beerId;
+    const beerDetails = {
+        title: req.body.title,
+        brewery: req.body.brewery,
+        description: req.body.description,
+        rating: req.body.rating
+     }; 
+     Beer.findByIdAndUpdate(beerId, beerDetails)
+    .then( () => {
+      res.redirect(`/beers/${beerId}`);
+    })
+    .catch( err => {
+      console.log("Error updating beer...", err);
+    });
+});
+
+router.post("/:beerId/delete", (req, res, next) => {
+    Beer.findByIdAndDelete(req.params.beerId)
+    .then( () => {
+        res.redirect("/beers");
+    })
+    .catch(err => {
+        console.log("Error deleting beer from DB", err)
+    })
+});
+
+     
 
 module.exports = router;
