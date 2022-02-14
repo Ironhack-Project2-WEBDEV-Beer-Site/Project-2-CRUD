7e18c6c03e662ac87651d1fe70592b9b157ed11c
Stephan Geyer <stephangeyer@Stephans-MacBook-Pro.local>
2022-02-14 09:21:19 +0100

    Initial Setup

---
+++ beer-crud-project/public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("beer-crud-project JS imported successfully!");
+  },
+  false
+);
---
+++ beer-crud-project/utils/consts.js
@@ -0,0 +1,3 @@
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/beer-crud-project";
+
+module.exports = MONGO_URI;
