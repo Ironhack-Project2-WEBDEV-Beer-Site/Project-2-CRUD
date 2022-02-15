7c688d94c577f2f62b2e1979701fc7450e056c86
stephan2311 <96991576+stephan2311@users.noreply.github.com>
2022-02-15 18:17:13 +0100

    Beerstyles added

--- beer-crud-project/models/Beer.model.js
+++ beer-crud-project/models/Beer.model.js
@@ -7,10 +7,11 @@ const beerSchema = new Schema(
     brewery: String,
     description: String,
     image: String,
+    beerstyle: ['Altbier', 'Amber ale', 'Barley wine', 'Berliner Weisse', 'Biere de Garde', 'Bitter', 'Blonde Ale', 'Bock', 'Brown ale', 'California Common/Steam Beer', 'Cream Ale', 'Dortmunder Export', 'Doppelbock', 'Dunkel', 'Dunkelweizen', 'Eisbock', 'Flanders red ale', 'Golden/Summer ale', 'Gose', 'Gueuze', 'Hefeweizen', 'Helles', 'India pale ale', 'Koelsch', 'Lambic', 'Light ale', 'Maibock/Helles bock', 'Malt liquor', 'Mild', 'Oktoberfestbier/Maerzenbier', 'Old ale', 'Oud bruin', 'Pale ale', 'Pilsener/Pilsner/Pils', 'Porter', 'Red ale', 'Roggenbier', 'Saison', 'Scotch ale', 'Stout', 'Schwarzbier', 'Vienna lager', 'Witbier', 'Weissbier', 'Weizenbock'],
     rating: {
-        type: Number,
-        min: 1,
-        max: 5,
+      type: Number,
+      min: 1,
+      max: 5,
     }
   },
   {
